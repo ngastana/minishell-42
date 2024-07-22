be28847d87bcbade992c1d30855f66611b590f9d
enekomb <enekomunoz@gmail.com>
2024-07-22 15:21:16 +0200

    quitau el leak

--- builtins/cd_utils.c
+++ builtins/cd_utils.c
@@ -16,6 +16,9 @@ void	update_env(t_mini *mini, char *old_env, char *new_env)
 {
 	char	*new_env_str;
 	int		i;
+	size_t	old_env_len;
+	size_t	new_env_len;
+	size_t	total_len;
 
 	i = 0;
 	while (mini->enviroment[i]
@@ -23,15 +26,16 @@ void	update_env(t_mini *mini, char *old_env, char *new_env)
 		i++;
 	if (mini->enviroment[i])
 	{
-		new_env_str = malloc(sizeof(char) * (ft_strlen(old_env)
-					+ ft_strlen(new_env) + 1));
+		old_env_len = ft_strlen(old_env);
+		new_env_len = ft_strlen(new_env);
+		total_len = old_env_len + new_env_len + 2;
+		new_env_str = malloc(total_len);
 		if (!new_env_str)
 			return ;
-		ft_strlcpy(new_env_str, mini->enviroment[i], ft_strlen(old_env) + 1);
-		new_env_str[ft_strlen(old_env)] = '=';
-		ft_strlcpy(new_env_str + ft_strlen(old_env) + 1, // leak
-			new_env, ft_strlen(new_env) + 1);
-		free (mini->enviroment[i]);
+		ft_strlcpy(new_env_str, old_env, old_env_len + 1);
+		new_env_str[old_env_len] = '=';
+		ft_strlcpy(new_env_str + old_env_len + 1, new_env, new_env_len + 1);
+		free(mini->enviroment[i]);
 		mini->enviroment[i] = new_env_str;
 	}
 }
@@ -66,3 +70,27 @@ void	go_home(t_mini *mini)
 		update_env(mini, "PWD", home);
 	g_status = 0;
 }
+
+/* void	update_env(t_mini *mini, char *old_env, char *new_env)
+{
+	char	*new_env_str;
+	int		i;
+
+	i = 0;
+	while (mini->enviroment[i]
+		&& (ft_strncmp(old_env, mini->enviroment[i], ft_strlen(old_env))))
+		i++;
+	if (mini->enviroment[i])
+	{
+		new_env_str = malloc(sizeof(char) * (ft_strlen(old_env)
+					+ ft_strlen(new_env) + 1));
+		if (!new_env_str)
+			return ;
+		ft_strlcpy(new_env_str, mini->enviroment[i], ft_strlen(old_env) + 1);
+		new_env_str[ft_strlen(old_env)] = '=';
+		ft_strlcpy(new_env_str + ft_strlen(old_env) + 1, // leak
+			new_env, ft_strlen(new_env) + 1);
+		free (mini->enviroment[i]);
+		mini->enviroment[i] = new_env_str;
+	}
+} */
